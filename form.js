9af76f5d3992eaebf0b66e8f338378dea530c9d8
Aryam <110060286+Aryam57@users.noreply.github.com>
2023-03-11 12:51:27 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,4 @@
+const  form = document.querySelector("form"),
+       nextBtn = form.querySelector(".nextBtm"),
+       backBtn = form.querySelector(".backBtn"),
+       allInput = form.querySelector(".first Input");
